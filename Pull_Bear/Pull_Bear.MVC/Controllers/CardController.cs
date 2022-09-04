d24b6b231b3d29ed923732e6b511e23a92ace5ca
al1yew <vasifja@code.edu.az>
2022-09-04 19:52:53 +0400

    Front to Back

---
+++ Pull_Bear/Pull_Bear.MVC/Controllers/CardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pull_Bear.MVC.Controllers
+{
+    public class CardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //MakeMain DeleteCard
+    }
+}
